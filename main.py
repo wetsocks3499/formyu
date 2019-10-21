7a853c93a665c2f43beeef360b7eed3f9110945d
C. Yerger <yerger.c.work@gmail.com>
2019-10-21 12:02:59 +0000

    Rectangular Prism Volume

--- main.py
+++ main.py
@@ -5,36 +5,45 @@ print('''
 
 welcome to formyu 
 
-~-~-~-~-~-~-~-~-~-
-''')
+~-~-~-~-~-~-~-~-~-''')
 ## fallback def
 def exit():
-	sys.exit()
+    sys.exit()
 def script():
-	print('''
-	''')
+    print('\n')
 ## declare
-	formula = input()
-	cubeVolumeVar = 'v -c'
-	HELP = 'help'
-	EXIT = 'exit'
+    formula = input()
+    cubeVolumeVar = 'v -c'
+    recPrisVolumeVar = 'v -r'
+    HELP = 'help'
+    EXIT = 'exit'
 ## useful def
-	def cubeVolumeFunction(): 
-		s = float(input('Side length of cube: '))
-		outprint = s ** 3
-		print('Volume = ', end='')
-		print(round(outprint, 2), 'units cubed.')
-		script()
-	def helpFunc():
-		f = open("man.txt", 'r')
-		print(f.read())
-		script()
+    def cubeVolumeFunction(): 
+        s = float(input('Side length of cube: '))
+        outprint = s ** 3
+        print('\nVolume = ')
+        print(round(outprint, 2), 'units cubed.')
+        script()
+    def recPrisVolumeFunc():
+        w = float(input('Width: '))
+        l = float(input('Length: '))
+        h = float(input('Height: '))
+        outprint = w * l * h
+        print('\nVolume = ')
+        print(round(outprint, 2), 'units cubed.')
+        script()
+    def helpFunc():
+        f = open("man.txt", 'r')
+        print(f.read())
+        script()
 ## redirect
-	if formula == cubeVolumeVar:
-		cubeVolumeFunction()
-	if formula == HELP:
-		helpFunc()
-	if formula == EXIT:
-		exit()
+    if formula == cubeVolumeVar:
+	    cubeVolumeFunction()
+    if formula == recPrisVolumeVar:
+        recPrisVolumeFunc()
+    if formula == HELP:
+	    helpFunc()
+    if formula == EXIT:
+	    exit()
 ## run
 script()
